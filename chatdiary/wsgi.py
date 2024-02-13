ad63a31bf4b66f13460b1289e2dd1f64f06a5782
Kimura-881014 <kimura.881014@gmail.com>
2024-02-13 16:40:24 +0900

    fix

--- chatdiary/wsgi.py
+++ chatdiary/wsgi.py
@@ -10,9 +10,9 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-# import sys
-# sys.path.append(os.path.dirname(os.path.abspath(__file__)))
-# sys.path.append(os.path.dirname(os.path.abspath(__file__)) + '/..')
+import sys
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(os.path.dirname(os.path.abspath(__file__)) + '/..')
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chatdiary.settings.development')
 
